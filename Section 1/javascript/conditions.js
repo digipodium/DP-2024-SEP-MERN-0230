39ad41f60d3128b05763a5bc6bf24341a71fe4f6
m-triple-m <triplem656@gmail.com>
2024-10-14 17:33:42 +0530

    conditions

---
+++ Section 1/javascript/conditions.js
@@ -0,0 +1,37 @@
+num = 234;
+
+if (num % 2 === 0) {
+    console.log('even');
+} else {
+    console.log('odd');
+}
+
+// WAP to check if person is eligible for DL
+age = 20;
+
+if (age >= 18) {
+
+    abc = 'something';
+    let a = 'local';
+    console.log(a);
+    const pi = 3.14;
+    // pi = 3.14;
+
+    console.log('Eligible for DL');
+} else {
+    console.log('Not eligible for DL');
+}
+
+console.log(abc);
+// console.log(a);
+
+// WAP to check if a number positive, negative or zero
+if (num > 0) {
+    console.log('positive');
+} else if (num < 0) {
+    console.log('negative');
+} else {
+    console.log('zero');
+}
+
+Number.isInteger();
