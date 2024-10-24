2c45aa9b456dbb0f13e7fa2fa2ca5aa03f422b21
m-triple-m <triplem656@gmail.com>
2024-10-24 15:32:35 +0530

    object

--- Section 1/javascript/object.js
+++ Section 1/javascript/object.js
@@ -71,7 +71,47 @@ const smartphoneArray = [
         price: 170000,
         colors: ['black', 'blue', 'white']
     },
+    {
+        brand: 'Samsung',
+        model: 'Galaxy s23',
+        price: 89000,
+        colors: ['gold', 'silver', 'white']
+    },
 ];
 
 console.log(smartphoneArray.length);
-// access price of 2nd smartphone
+
+// filter all smartphones with price less than 50000
+
+const budgetPhones = smartphoneArray.filter((phone) => {
+    return phone.price < 50000;
+});
+
+console.log(budgetPhones);
+
+// filter all phones with blue color
+console.log(['a', 'b', 'c'].includes('d'));
+
+const bluePhones = smartphoneArray.filter((phone) => {
+    return phone.colors.includes('blue');
+});
+console.log(bluePhones);
+
+
+const brands = smartphoneArray.map((phone) => {
+    return phone.brand;
+});
+
+console.log(brands);
+
+const keyword = 'sam';
+
+// const results = smartphoneArray.filter((phone) => {
+//     return phone.brand.toLowerCase() === keyword.toLowerCase();
+// });
+
+const results = smartphoneArray.filter((phone) => {
+    return phone.brand.toLowerCase().includes(keyword.toLowerCase());
+});
+
+console.log(results);
