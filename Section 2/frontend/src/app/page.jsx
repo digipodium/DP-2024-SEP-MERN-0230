a0cf078fba5babed5fd2f6bd487f7139b3df1e20
m-triple-m <triplem656@gmail.com>
2024-10-27 14:34:30 +0530

    creating pages

---
+++ Section 2/frontend/src/app/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+
+
+  return (
+    <div>
+      <h1 className='text-center my-5 font-bold text-3xl'>Home Page</h1>
+      <p style={{ color: 'red', fontWeight: 'bold', fontSize: 50 }}>
+        JSX is the combination of html and js
+      </p>
+
+      <input type="text" />
+      <br />
+      <hr />
+    </div>
+  )
+}
+
+export default Home;
