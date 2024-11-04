7a9e04f04460c417324567150f7f35ca3b4b804f
m-triple-m <triplem656@gmail.com>
2024-11-04 16:31:56 +0530

    navbar

--- Section 2/frontend/src/app/layout.jsx
+++ Section 2/frontend/src/app/layout.jsx
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,7 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <h1>Website Header</h1>
+        <Navbar />
         {children}
         <h1>Website Footer</h1>
       </body>
