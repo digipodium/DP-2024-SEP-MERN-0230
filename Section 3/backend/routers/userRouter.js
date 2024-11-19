be27763f4ccb3c62f0aa06f4ff5cf089e9f143a2
m-triple-m <triplem656@gmail.com>
2024-11-19 15:59:58 +0530

    delete and update operation

--- Section 3/backend/routers/userRouter.js
+++ Section 3/backend/routers/userRouter.js
@@ -47,8 +47,25 @@ router.get('/getbyid/:id', (req, res) => {
         });
 });
 
-// getbyid
-// delete
-// update
+router.delete('/delete/:id', (req, res) => {
+    Model.findByIdAndDelete(req.params.id)
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.put('/update/:id', (req, res) => {
+    Model.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 
 module.exports = router;
