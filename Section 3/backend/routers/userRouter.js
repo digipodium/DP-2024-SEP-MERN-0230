ecf5f2a4419b0d6161fd573c8c8132f361dad698
m-triple-m <triplem656@gmail.com>
2024-11-19 15:24:35 +0530

    read operation

--- Section 3/backend/routers/userRouter.js
+++ Section 3/backend/routers/userRouter.js
@@ -18,12 +18,33 @@ router.post('/add', (req, res) => {
 // getall
 router.get('/getall', (req, res) => {
     Model.find()
-    .then((result) => {
-        res.status(200).json(result);
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// : denotes url parameter
+router.get('/getbycity/:city', (req, res) => {
+    Model.find({ city: req.params.city })
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/getbyid/:id', (req, res) => {
+    Model.findById(req.params.id)
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 // getbyid
